8eb822c04d246c254268eb26544ae5355e67ba01
KDH <lococo0425@naver.com>
2024-03-27 15:50:35 +0900

    240327

--- src/main/reactapp/src/component/layout/Header.jsx
+++ src/main/reactapp/src/component/layout/Header.jsx
@@ -1,9 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header(props){
+    // 0. 로그인정보 state변수
+    const [loginInfo , setloginInfo] = useState('');
+
+    // 컴포넌트 생성시 axios 실행해서 로그인 회원정보 호출
+    // 1. 컴포넌트가 실행될때 1번 axios 요청보내서 회원정보 가져온다
+    useEffect(()=>{
+        axios.get('/member/login/info/get.do')
+        .then((r)=>{ console.log(r);
+        setloginInfo(r.data);
+        })
+        .catch((e)=>{ console.log(e);})
+    } , []);
     
     return(<>
         <div>
+            { loginInfo && <span> {loginInfo.memail} 님</span>}
             <ul>
                 <li><Link to="/">홈</Link></li>
                 <li><Link to="/member/signup">회원가입</Link></li>
--- src/main/reactapp/src/component/member/Login.jsx
+++ src/main/reactapp/src/component/member/Login.jsx
@@ -1,3 +1,30 @@
+import axios from "axios";
+
 export default function Login(props){
-    return(<><h3>로그인페이지</h3></>);
+
+    const onLogin = ()=>{
+        // 1. 전송할 폼 가져오기
+        const loginFrom = document.querySelector('#loginFrom');
+        // 2. 데이터폼 으로 변환
+        const loginFromData = new FormData(loginFrom);
+        // 3. 서버와 통신
+        axios.post('/member/login/post.do',loginFromData)
+        .then((response)=>{ console.log(response);
+            if(response.data){
+                alert('로그인성공');
+            }else{alert("로그인실패");}
+            window.location.href="/";
+
+        })
+        .catch((e)=>{console.log(e);})
+
+    }
+
+    return(<>
+        <form id="loginFrom">
+            이메일 : <input type="text" name="memail" />
+            비밀번호 : <input type="password" name="mpassword" />
+            <button type="button" onClick={ onLogin }>로그인</button>
+        </form>
+    </>);
 }
